04cb7c32708ffcc5f94eac0906939543a06eafdf
jubeXyz <julibebensee@gmail.com>
2024-02-26 10:29:11 +0100

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:grocery_task/common/data/cart_repository.dart';
 import 'package:grocery_task/common/data/wishlist_repository.dart';
@@ -7,10 +8,15 @@ import 'package:grocery_task/features/overview/data/category_repository.dart';
 import 'package:grocery_task/features/overview/data/product_repository.dart';
 import 'package:grocery_task/features/overview/presentation/controllers/category_controller.dart';
 import 'package:grocery_task/features/overview/presentation/controllers/product_controller.dart';
+import 'package:grocery_task/firebase_options.dart';
 import 'package:grocery_task/grocery_app.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [
